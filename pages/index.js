131592543270b23427c89ef716223a32441caedc
Brian Thomas <brianthomas571@gmail.com>
2022-05-01 16:14:30 -0500

    fetching sanity products, added heroBanner

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { Product, FooterBanner, HeroBanner } from "../components/index.js";
-const index = () => {
+import { client } from "../lib/client";
+
+const Home = ({ productsData, bannerData }) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many kinds</p>
       </div>
       <div className="products-container">
-        {["product 1", "product 2"].map((product) => product)}
+        {productsData?.map((product) => product.name)}
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
 };
 
-export default index;
+export const getServerSideProps = async () => {
+  const query = `*[_type == "product"]`;
+  const productsData = await client.fetch(query);
+
+  const bannerQuery = `*[_type == "banner"]`;
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { productsData, bannerData },
+  };
+};
+
+export default Home;
