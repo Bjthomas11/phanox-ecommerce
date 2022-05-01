73c57ba8b4cbde773420b871c6d6a69686a012bb
Brian Thomas <brianthomas571@gmail.com>
2022-05-01 15:33:02 -0500

    added all dummy components

---
+++ components/index.js
@@ -0,0 +1,8 @@
+// exporting all components
+export { default as Footer } from "./footer/Footer";
+export { default as FooterBanner } from "./footer-banner/FooterBanner";
+export { default as Cart } from "./cart/Cart";
+export { default as HeroBanner } from "./hero-banner/HeroBanner";
+export { default as Layout } from "./layout/Layout";
+export { default as Nav } from ".//nav/Nav";
+export { default as Product } from "./product/Product";
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Product, FooterBanner, HeroBanner } from "../components/index.js";
 const index = () => {
-  return <div>index</div>;
+  return (
+    <>
+      HeroBanner
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many kinds</p>
+      </div>
+      <div className="products-container">
+        {["product 1", "product 2"].map((product) => product)}
+      </div>
+      Footer
+    </>
+  );
 };
 
 export default index;
